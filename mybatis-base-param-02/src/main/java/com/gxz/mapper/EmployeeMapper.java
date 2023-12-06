3fdfe8724cad5163791cba1a17ca96cdffb04548
gxz_pc <xizhuogao@foxmail.com>
2023-12-06 13:54:39 +0800

    P76 mybatis over!

---
+++ mybatis-base-param-02/src/main/java/com/gxz/mapper/EmployeeMapper.java
@@ -0,0 +1,31 @@
+package com.gxz.mapper;
+
+import com.gxz.pojo.Employee;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmployeeMapper {
+     //根据id查询员工信息
+     Employee queryById(Integer id);
+
+      //场景1：根据id删除员工信息
+     int deleteById(Integer id);
+     //根据工资查询员工信息
+     List<Employee> queryBySalary(Double salary);
+
+     //场景2：插入员工数据 【实体对象】
+     int insertEmp(Employee employee);
+
+     //场景3：根据员工姓名和工资查询员工信息
+     List<Employee> queryByNameAndSalary(@Param("a")String name, @Param("b") Double salary);
+
+     //场景4：插入员工数据，传入的是一个map(name=员工的名字,salary=员工的薪水)
+     int insertEmpMap(Map data);
+
+
+
+
+
+}
