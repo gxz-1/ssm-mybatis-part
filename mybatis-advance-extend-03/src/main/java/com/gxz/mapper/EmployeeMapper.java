667a52f3b0da6baf57b98c8a8a25698baa88f496
gxz_pc <xizhuogao@foxmail.com>
2023-12-12 13:32:12 +0800

    P99 mybatis all over!

---
+++ mybatis-advance-extend-03/src/main/java/com/gxz/mapper/EmployeeMapper.java
@@ -0,0 +1,13 @@
+package com.gxz.mapper;
+
+import com.gxz.pojo.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeMapper {
+
+
+    List<Employee> queryList();
+
+}
---
+++ mybatis-advance-extend-03/src/main/java/com/gxz/mapper/UserMapper.java
@@ -0,0 +1,5 @@
+package com.gxz.mapper;
+
+
+public interface UserMapper {
+}
